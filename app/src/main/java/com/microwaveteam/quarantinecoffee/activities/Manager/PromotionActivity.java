1795347e0b177246fd54c87d5bebc67a5b374208
Thư Nguyễn <quynhthuqn19@gmail.com>
2021-08-07 01:15:36 +0700

    add manager acivity

---
+++ app/src/main/java/com/microwaveteam/quarantinecoffee/activities/Manager/PromotionActivity.java
@@ -0,0 +1,16 @@
+package com.microwaveteam.quarantinecoffee.activities.Manager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.microwaveteam.quarantinecoffee.R;
+
+public class PromotionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promotion);
+    }
+}
