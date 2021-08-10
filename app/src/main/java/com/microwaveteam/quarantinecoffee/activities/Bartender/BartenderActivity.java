04803c72633b23693aa47210d00388b87ce50881
Phong200200 <quocphong20022000@gmail.com>
2021-08-10 12:58:17 +0700

    phong

--- app/src/main/java/com/microwaveteam/quarantinecoffee/activities/Bartender/BartenderActivity.java
+++ app/src/main/java/com/microwaveteam/quarantinecoffee/activities/Bartender/BartenderActivity.java
@@ -1,12 +1,30 @@
 package com.microwaveteam.quarantinecoffee.activities.Bartender;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.microwaveteam.quarantinecoffee.Helper.NavigationDrawer;
 import com.microwaveteam.quarantinecoffee.R;
 import com.microwaveteam.quarantinecoffee.activities.Waiter.WaiterActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class BartenderActivity extends AppCompatActivity {
 
@@ -14,6 +32,43 @@ public class BartenderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.b_activity_bartender);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Order");
+        String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+
+        myRef.child(currentDate).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Toast.makeText(BartenderActivity.this
+                        ,snapshot.getKey().toString() +" - "+ snapshot.getValue().toString()
+                        ,Toast.LENGTH_LONG);
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
 
     }
 }
--- app/src/main/java/com/microwaveteam/quarantinecoffee/activities/Waiter/WaiterActivity.java
+++ app/src/main/java/com/microwaveteam/quarantinecoffee/activities/Waiter/WaiterActivity.java
@@ -7,26 +7,48 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.AlertDialog;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.microwaveteam.quarantinecoffee.Helper.NavigationDrawer;
 import com.microwaveteam.quarantinecoffee.R;
 import com.microwaveteam.quarantinecoffee.activities.LoginActivity;
+import com.microwaveteam.quarantinecoffee.activities.MainActivity;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class WaiterActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    Button btnAddtoCart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.w_activity_waiter);
+        binding();
+        onClick();
+    }
+
+    private void binding() {
+        btnAddtoCart = findViewById(R.id.w_btn_confirm_order);
 
 
-        
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigationView);
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(WaiterActivity.this,
@@ -34,21 +56,41 @@ public class WaiterActivity extends AppCompatActivity implements NavigationView.
                 R.string.open,
                 R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
         actionBarDrawerToggle.syncState();
+
 
         navigationView.bringToFront();
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.getCheckedItem();
+
+
     }
 
+    private void onClick() {
+        btnAddtoCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String,String> map = new HashMap<String,String>();
+                map.put("Cafe","2");
+                map.put("cafe da","4");
 
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("Order");
+                String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+
+                myRef.child(currentDate).push().setValue(map);
+
+                Toast.makeText(WaiterActivity.this,"Oke",Toast.LENGTH_LONG);
+            }
+        });
+    }
 
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.help_navItem_exit:
-
                 Intent it = new Intent(WaiterActivity.this, LoginActivity.class);
                 startActivity(it);
                 break;
@@ -60,5 +102,6 @@ public class WaiterActivity extends AppCompatActivity implements NavigationView.
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
+
 
 }
